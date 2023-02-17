71e1925a1dcede550be667ef9ad88e7356c52391
Stephan <obvilion@gmail.com>
2023-02-18 00:20:45 +0100

    Added a few configurations

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    open: true,
+    host: true,
     watch: {
       usePolling: true,
     },
